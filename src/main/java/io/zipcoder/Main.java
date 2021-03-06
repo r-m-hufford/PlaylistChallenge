75c7629cb057093de4acb12e0db6b01989f01483
r-m-hufford <ryan@rmhufford.com>
2021-03-06 12:20:06 -0500

    passed tests. Created setters for psuedo last/next buttons

---
+++ src/main/java/io/zipcoder/Main.java
@@ -0,0 +1,8 @@
+package io.zipcoder;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,43 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int steps = 0;
+
+        for (int i = startIndex; i < this.playList.length - 1; i++) {
+            steps++;
+            if (this.playList[i] == selection) {
+                break;
+            }
+        }
+        if (steps > (this.playList.length / 2)) {
+            steps = playList.length - steps;
+        }
+        return steps;
+    }
+
+    public int setNextSong(String currentSong) {
+        int nextSong = 0;
+
+        for (int i = 0; i < this.playList.length - 1; i++) {
+            if (this.playList[i] == currentSong) {
+                nextSong = i + 1;
+            }
+        }
+        return nextSong;
+    }
+
+    public int setLastSong(String currentSong) {
+        int lastSong = 0;
+
+        for (int i = 0; i < this.playList.length - 1; i++) {
+            if (this.playList[i] == currentSong) {
+                lastSong = i - 1;
+            }
+        }
+        return lastSong;
+    }
+
+    public void setSong() {
+
     }
 }
